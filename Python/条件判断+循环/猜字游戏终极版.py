53b8ef8ea2b22a36920dd7e4983eb6b54c0b4747
Qing Zhu <zhuqing161215@163.com>
2025-03-06 16:00:12 +0800

    Update and rename test4_1.py to 猜字游戏终极版.py

--- Python/条件判断+循环/猜字游戏终极版.py
+++ Python/条件判断+循环/猜字游戏终极版.py
@@ -25,4 +25,20 @@ if guess_num!=num:
         print('我了个逗，不是哥们你真会啊！')
 else:
     print('我了个逗，不是哥们你真会啊！')
-
+# while循环版
+import random
+num=random.randint(1, 10)
+print("""听说你最近学了一套魔法？我心中想一个(1-10)的数，你把住我的脉就可以确定这个数？""")
+print('“你咋知道嘞，那我给你小露一手！”')
+# 存储猜了几次
+count=1
+# 获取输入值
+guess_num=int(input('观其手微微贴在脉上，作沉思状~~,能猜中不？\n')) 
+while guess_num!=num:
+    if guess_num<num:
+        print('小了！')
+    else:
+        print('这么大！你咋不说10以外的呢')
+    guess_num=int(input('观其擦了擦手，再次作沉思状~~\n')) 
+    count+=1
+print(f'你小子还真有两下哟~，只用了{sum}次就猜中了')
