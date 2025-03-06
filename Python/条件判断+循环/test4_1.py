91a4d602665a53a7b2c6af8502567907c783aa5d
Qing Zhu <zhuqing161215@163.com>
2025-03-06 15:07:21 +0800

    Update test4_1.py

--- Python/条件判断+循环/test4_1.py
+++ Python/条件判断+循环/test4_1.py
@@ -1,13 +1,3 @@
-# 成年人判断
-print('铁铁，欢迎来到川大儿童游乐园，我们以身高和年龄为标准，确定您的票种。')
-age=input('您的年龄？\n')
-height=int(input('您的身高多少厘米?\n'))
-age=int(age)
-if (age<=18)and(height<=130):
-    print('根据你的信息，你可以购买儿童票')
-    print('祝您游玩愉快')
-else:
-    print('滚泥马的，这是儿童乐园，瞎几把凑什么热闹呐！')
 # 猜字游戏
 import random
 num=random.randint(1, 10)
